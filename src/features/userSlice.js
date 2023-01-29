788a223a3cf0323c68cb4a51e256306f01212a7d
saymanrabbi <mdrabbi.asm@gmail.com>
2023-01-29 16:55:25 +0600

    setup redux

---
+++ src/features/userSlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+    email: null,
+    name: null,
+    token: null
+}
+
+const userSlice = createSlice({
+    name: "user",
+    initialState,
+    reducers: {
+        login: (state, action) => {
+            state.email = action.payload.email
+            state.name = action.payload.name
+            state.token = action.payload.token
+        },
+        logout: (state) => {
+            state.email = null
+            state.name = null
+            state.token = null
+        },
+        register: (state, action) => {
+            state.email = action.payload.email
+            state.name = action.payload.name
+            state.token = action.payload.token
+        }
+    }
+})
+
+export const { login, logout, register } = userSlice.actions
+export default userSlice.reducer
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from "react-router-dom";
 import App from './App';
+import store from './app/store';
 import './index.css';
 const queryClient = new QueryClient()
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
    <QueryClientProvider client={queryClient}>
+    <Provider store={store}>
         <BrowserRouter>
            <App />
         </BrowserRouter>
+        </Provider>
     </QueryClientProvider>
   </React.StrictMode>
 );
